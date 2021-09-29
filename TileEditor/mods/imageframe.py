586cb670d76cb3bafcc919d7266c03d8d2e7adaa
Multilingual-Coder <71729368+Multilingual-Coder@users.noreply.github.com>
2021-09-28 22:27:00 -0400

    Update imageframe.py

--- TileEditor/mods/imageframe.py
+++ TileEditor/mods/imageframe.py
@@ -13,6 +13,8 @@
 
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#    
+#    Contact the developer at codermultilingual@gmail.com
 import tkinter as tk
 import tkinter.filedialog
 from tkinter import RIDGE, FLAT
